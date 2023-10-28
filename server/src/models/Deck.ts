380143184978c7d8f2d52ea59d54a707a6fb164f
Janeil Tan <janeiltan@Janeils-MacBook-Air.local>
2023-10-28 13:46:59 +1300

    Psuedo code lines of code for Schema

--- server/src/models/Deck.ts
+++ server/src/models/Deck.ts
@@ -1,12 +1,15 @@
 import mongoose from 'mongoose';
 
+// The Schema object is used to define the structure and data types for documents in MongoDB
 const Schema = mongoose.Schema;
-const ObjectId = Schema.ObjectId;
+// const ObjectId = Schema.ObjectId;
 
+// defines a new Mongoose schema for a "Deck"
 const DeckSchema = new Schema({
   title: String,
 });
 
+// Assigns the result of calling mongoose.model()
 const DeckModel = mongoose.model('Deck', DeckSchema);
 
 export default DeckModel;
