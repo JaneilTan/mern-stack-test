718b30a146361eb80bf71e1ac36f42b02687a442
Janeil Tan <aguilarjang@gmail.com>
2023-11-05 18:55:36 +1300

    Psuedo code delete endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -26,6 +26,15 @@ app.post("/decks", async (req: Request, res: Response) => {
     res.json(createdDeck);
 });
 
+app.delete("/decks/:deckId", async (req: Request, res: Response) => {
+    // TODO:
+    // 1. get the deck id from the url
+
+    // 2. delete the deck from mongo
+
+    // 3. return the deleted deck to the user who made the request
+})
+
 mongoose.connect(process.env.MONGO_URL!).then(() => {
         console.log(`listening on port ${PORT}`);
         app.listen(PORT);
