ae2b99b80fab6591f497cb2865cc2d8f6896e6f0
Janeil Tan <aguilarjang@gmail.com>
2023-11-06 15:30:46 +1300

    Remove pseudo code for clean code

--- server/src/index.ts
+++ server/src/index.ts
@@ -27,12 +27,8 @@ app.post("/decks", async (req: Request, res: Response) => {
 });
 
 app.delete("/decks/:deckId", async (req: Request, res: Response) => {
-    // TODO:
-    // 1. get the deck id from the url
     const deckId = req.params.deckId;
-    // 2. delete the deck from mongo
     const deck = await Deck.findByIdAndDelete(deckId);
-    // 3. return the deleted deck to the user who made the request
     res.json(deck);
 });
 
