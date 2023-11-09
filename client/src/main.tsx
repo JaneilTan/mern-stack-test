cb2caf9c8dc46417b9154e11613d786a3bf2c20b
Janeil Tan <aguilarjang@gmail.com>
2023-11-10 09:14:48 +1300

    Add /deck path

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,12 +6,18 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import './index.css'
+import Deck from './Deck.tsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
   },
+  {
+    path: "/decks/:deckId",
+    element: <Deck />,
+  },
+  
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
