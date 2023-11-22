8c292a104f69eb0f32fdd1c46720ee3a883c4b6a
Janeil Tan <aguilarjang@gmail.com>
2023-11-23 11:40:29 +1300

    Update cors origin

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,22 +5,26 @@ import express, { Request, Response } from "express";
 import mongoose from 'mongoose';
 import cors from 'cors';
 import Deck from "./models/Deck";
-import { getDecksController } from "./controllers/getDeckController";
+import { getDecksController } from "./controllers/getDecksController";
 import { createdDeckController } from "./controllers/createDeckController";
 import { deleteDeckController } from "./controllers/deleteDeckController";
+import { getDeckController } from "./controllers/getDeckController";
+import { createCardForDeckController } from "./controllers/createCardForDeckController";
 
 const PORT = 3000;
  
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "*",
+}));
 app.use(express.json());
 
 app.get('/decks', getDecksController);
-
 app.post("/decks", createdDeckController);
-
 app.delete("/decks/:deckId", deleteDeckController);
+app.get('/decks', getDeckController);
+app.post("/decks/:deckId/cards", createCardForDeckController);
 
 mongoose.connect(process.env.MONGO_URL!).then(() => {
         console.log(`listening on port ${PORT}`);
